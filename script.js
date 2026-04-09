55e87544a22465a192c65af755ccef5a5def5e99
aminam-cd <ameena3ali@gmail.com>
2026-04-09 15:05:04 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ const adhkarData = {
         arabic: "لَا إِلَهَ إِلَّا اللَّهُ وَحْدَهُ لَا شَرِيكَ لَهُ، لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيرٌ",
         meaning: "None has the right to be worshipped except Allah alone",
         source: "رواه البخاري ومسلم",
-        repeat: 10,
+        repeat: 100,
         virtue: "عِدل عتق ١٠ رقاب"
       },
       {
